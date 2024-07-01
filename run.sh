8037b88fb00b2fcf772eed4d8e9b57f15a04d399
satpal82bhandari <satpal82bhandari@gmail.com>
2024-07-01 20:33:56 +0530

    INitial working version

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ export VERILATOR_ROOT="/usr/bin/verilator"
 export PATH=$VERILATOR_ROOT/bin:$PATH
 
 # Your OPENAI Key
-export OPENAI_API_KEY='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'
+export OPENAI_API_KEY='OPEN_API_KEY'
 
 python3.10 run.py
 
